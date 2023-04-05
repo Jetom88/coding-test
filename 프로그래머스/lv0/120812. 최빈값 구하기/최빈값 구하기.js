aef6203ca88a74de146d7776e9f8b14e2133b873
Hyeri Shin <64053930+Jetom88@users.noreply.github.com>
2023-04-05 22:19:01 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,13 +1,27 @@
 function solution(array) {
-    const mapped = {}
-    
-    array.forEach(item => mapped[item] = (mapped[item] ?? 0) + 1)
-    
-    const max = Math.max(...Object.values(mapped))
-    
-    const result = Object.entries(mapped).filter(([key, value]) => value === max).map(x=>Number(x[0]))
-    
-    if (result.length > 1) return -1
-    
-    return result[0]
+  const mapping = {}
+  
+  for(let i = 0; i < array.length; i++){
+    if(mapping[array[i]] === undefined){
+      mapping[array[i]] = 1
+    } else {
+      mapping[array[i]]++
+    }
+  }
+  
+  let maxCount = 0;
+  let mode = -1
+ 
+  for(const key in mapping){
+    if(mapping[key] > maxCount){
+      maxCount = mapping[key]
+      mode = key
+    } else if(mapping[key] === maxCount){
+      mode = -1
+    }
+  }
+  
+  return Number(mode)
 }
+
+solution([1, 1, 2, 2])
