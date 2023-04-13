760e1da075c26db66af97bdde02f9aeb035aa6f7
Hyeri Shin <64053930+Jetom88@users.noreply.github.com>
2023-04-13 21:23:01 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.36 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,8 +1,16 @@
 function solution(s) {
-  const num = s.split(" ").map(Number)
-
-  const min = Math.min(...num)
-  const max = Math.max(...num)
-  
-  return `${min} ${max}`
+    const num = s.split(' ');
+    
+    let min = Number(num[0]);
+    let max = Number(num[0]);
+    
+    for (let i = 1; i < num.length; i++) {
+        if (Number(num[i]) < min) {
+            min = num[i];
+        }
+        if (Number(num[i]) > max) {
+            max = num[i];
+        }
+    }
+    return min + ' ' + max;
 }
