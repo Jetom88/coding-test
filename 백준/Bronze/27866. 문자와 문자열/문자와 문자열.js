24adfbe9e0b46e4aea42edb9664406f45c8e1ac1
Hyeri Shin <64053930+Jetom88@users.noreply.github.com>
2024-03-28 12:10:02 +0900

    [Bronze V] Title: 문자와 문자열, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,12 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const splitFlag = process.platform === "linux" ? "\n" : "\r\n";
+
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(splitFlag);
+
+const changeNumber = Number(input[1]);
+
+console.log(input[0].charAt(changeNumber - 1));
