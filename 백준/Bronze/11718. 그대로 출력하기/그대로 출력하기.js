c6f3f809cecbdf1dc9ea45ad36ccc6e5edd79612
Hyeri Shin <64053930+Jetom88@users.noreply.github.com>
2024-04-08 16:55:30 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,10 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const splitFlag = process.platform === "linux" ? "\n" : "\r\n";
+
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(splitFlag);
+
+console.log(input.join(splitFlag));
