b480cd49d55ae3d7b292d044ac116b332f179137
Hyeri Shin <64053930+Jetom88@users.noreply.github.com>
2024-04-10 12:40:10 +0900

    [Bronze I] Title: 단어 공부, Time: 264 ms, Memory: 16508 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -2,15 +2,15 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
 
 const input = require("fs").readFileSync(filePath).toString().trim();
 
-const changeLowerCase = input.toLocaleLowerCase();
+const lowerCasedInput = input.toLocaleLowerCase();
 
 const obj = {};
 const count = [];
 
 let maxValue = 0;
 
-for (const el of changeLowerCase) {
-  obj[el] === undefined ? (obj[el] = +1) : obj[el]++;
+for (const el of lowerCasedInput) {
+  obj[el] = (obj[el] || 0) + 1;
 }
 
 maxValue = Math.max(...Object.values(obj));
@@ -22,3 +22,5 @@ for (const key in obj) {
 }
 
 console.log(count.length > 1 ? "?" : count[0].toLocaleUpperCase());
+
+//조건문 간결 + 변수명 수정
