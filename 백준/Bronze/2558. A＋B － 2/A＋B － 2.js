44e977e4c50984c7d3234506c16eb1262b78f60a
Hyeri Shin <64053930+Jetom88@users.noreply.github.com>
2024-04-15 22:20:58 +0900

    [Bronze V] Title: A+B - 2, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const splitFlag = process.platform === "linux" ? "\n" : "\r\n";
+
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(splitFlag)
+  .map(Number);
+
+console.log(input[0] + input[1]);
